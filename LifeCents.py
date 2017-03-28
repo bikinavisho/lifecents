59ef44f57cd8de4f2b99b6a37de8960f6bb8b003
Bianca <brs41@txstate.edu>
2017-03-27 22:58:06 -0500

    Implemented skeleton of a database as well as the beginnings of user logins.

--- LifeCents.py
+++ LifeCents.py
@@ -1,7 +1,34 @@
 from flask import Flask
 import flask
+import flask_sqlalchemy
+import dataBaser
+import base64, os
 
 app = Flask(__name__)
+app.config.from_pyfile('settings.py')
+db = flask_sqlalchemy.SQLAlchemy(app)
+
+
+@app.before_request
+def setup_user():
+    """
+    Figure out if we have an authorized user, and look them up.
+    This runs for every request, so we don't have to duplicate code.
+    """
+    if 'auth_user' in flask.session:
+        user = dataBaser.User.query.get(flask.session['auth_user'])
+        if user is None:
+            # old bad cookie, no good
+            del flask.session['auth_user']
+        # save the user in `flask.g`, which is a set of globals for this request
+        flask.g.user = user
+
+
+@app.before_request
+def setup_csrf():
+    # make a cross-site request forgery preventing token
+    if 'csrf_token' not in flask.session:
+        flask.session['csrf_token'] = base64.b64encode(os.urandom(32)).decode('ascii')
 
 
 # This signifies that when you navigate to the URL / on our homepage (lcents.com/) it will run this function
---
+++ dataBaser.py
@@ -0,0 +1,11 @@
+from LifeCents import db, app
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(20))
+    pw_hash = db.Column(db.String(64))
+
+
+db.create_all(app=app)
+

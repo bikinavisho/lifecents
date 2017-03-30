f69235fe6a08783e3e074b720deeba08a13e4ab7
Bianca <brs41@txstate.edu>
2017-03-29 19:46:42 -0500

    Implementation of user logins.

--- LifeCents.py
+++ LifeCents.py
@@ -2,7 +2,7 @@ from flask import Flask
 import flask
 import flask_sqlalchemy
 import dataBaser
-import base64, os
+import base64, os, bcrypt, re
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
@@ -39,6 +39,98 @@ def render_homepage():
         # Ex: flask.render_template('index.html', var1, var2)
         #       and in the html file you would put {{ var1 }} to print its value
     return flask.render_template('index.html')
+
+
+# ================================
+#           LOGIN LOGIC
+# ================================
+@app.route('/login')
+def login_page():
+    return flask.render_template('login.html')
+
+
+@app.route('/register')
+def register_page():
+    return flask.render_template('register.html')
+
+
+@app.route('/login', methods=['POST'])
+def handle_login():
+    # POST request to /login - check user
+    name = flask.request.form['username']
+    password = flask.request.form['password']
+    # try to find user
+    user = dataBaser.User.query.filter_by(name=name).first()
+
+    if user is not None:
+        # hash the password the user gave us
+        # for verifying, we use their real hash as the salt
+        pw_hash = bcrypt.hashpw(password.encode('utf8'), user.pw_hash)
+        # is it good?
+        if pw_hash == user.pw_hash:
+            # yay!
+            flask.session['auth_user'] = user.id
+            # And redirect to '/', since this is a successful POST
+            return flask.redirect(flask.url_for('render_homepage'), code=303)
+
+    # if we got this far, either username or password is no good
+    # For an error in POST, we'll just re-show the form with an error message
+    return flask.render_template('login.html', error_msg="Invalid username or password")
+
+
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    name = flask.request.form['username']
+    password = flask.request.form['password']
+
+    # do the passwords match?
+    error = None
+    if password != flask.request.form['confirm_password']:
+        error = "Passwords don't match"
+    # is the login ok?
+    if len(name) > 60:
+        error = "Username too long"
+    if not re.match(r"^[A-Za-z0-9\.\+_-]*$", name):
+        error = "Username contains invalid characters"
+
+    # search for existing user
+    existing = dataBaser.User.query.filter_by(name=name).first()
+
+    if existing is not None:
+        # oops, found your doppelgänger
+        error = "Username already taken"
+
+    if error:
+        return flask.render_template('register.html', error_msg=error)
+
+    # create user
+    user = dataBaser.User()
+    user.name = name
+    # hash password
+    user.pw_hash = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt(15))
+
+    # save user
+    db.session.add(user)
+    db.session.commit()
+
+    flask.session['auth_user'] = user.id
+
+    # It's all good!
+    return flask.redirect(flask.url_for('render_homepage'), 303)
+
+
+@app.route('/logout')
+def handle_logout():
+    # user wants to say goodbye, just forget about them
+    del flask.session['auth_user']
+    del flask.g.user
+    # redirect to specified source URL, or / if none is present
+    return flask.redirect(flask.url_for('render_homepage'), code=303)
+
+
+# ================================
+#         END LOGIN LOGIC
+# ================================
 
 
 # This runs the application which is our website

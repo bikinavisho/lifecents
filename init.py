1dcabd3ec0284738ae20e9e00b8c06e2f1fcc20c
Bianca <brs41@txstate.edu>
2017-04-18 16:39:26 -0500

    Update settings.py

--- init.py
+++ init.py
@@ -1,10 +1,8 @@
 import flask
 import flask_sqlalchemy
-import os
 from flask_socketio import SocketIO
 
 app = flask.Flask(__name__)
 app.config.from_pyfile('settings.py')
-app.config.from_object(os.environ['SECRET_KEY'])
 db = flask_sqlalchemy.SQLAlchemy(app)
 socket_io = SocketIO(app)

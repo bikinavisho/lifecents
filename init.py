70052fa8420195f7ff11b0e4bceb11b9308e8d08
Bianca <brs41@txstate.edu>
2017-04-18 16:31:38 -0500

    Configuration

--- init.py
+++ init.py
@@ -5,6 +5,6 @@ from flask_socketio import SocketIO
 
 app = flask.Flask(__name__)
 app.config.from_pyfile('settings.py')
-app.config.from_object(os.environ['SECRET_KEY'], os.environ['ADMIN_PASSWORD'])
+app.config.from_object(os.environ['SECRET_KEY'])
 db = flask_sqlalchemy.SQLAlchemy(app)
 socket_io = SocketIO(app)
